0cbd41ca21d8d9a089d7cf3d0e072d8fa3b1ddda
Lorenzo Palmes <lpalmes@gmail.com>
2025-12-10 18:42:25 +0000

    wip

---
+++ app/no_cache/sequential/[amount]/page.tsx
@@ -0,0 +1,43 @@
+import { fetchWineList, fetchWine } from "@/lib/api"
+
+async function test(id: number) {
+	const start = Date.now();
+	const wine = await fetchWine(id);
+	return {
+		...wine,
+		duration: Date.now() - start
+	}
+}
+
+export default async function Page({ params }: { params: Promise<{ amount: string }> }) {
+	const { amount: amountStr } = await params
+	const amount = parseInt(amountStr)
+
+	let wineList = await fetchWineList()
+	wineList = wineList.slice(0, amount);
+
+	const wines = []
+
+	for (const wine of wineList) {
+		wines.push(await test(wine.id));
+	}
+
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Duration (ms)</th>
+					<th>Wine</th>
+				</tr>
+			</thead>
+			<tbody>
+				{wines.map(item =>
+					<tr key={item.id}>
+						<td>{item.duration}</td>
+						<td>{item.wine}</td>
+					</tr>
+				)}
+			</tbody>
+		</table>
+	)
+}
